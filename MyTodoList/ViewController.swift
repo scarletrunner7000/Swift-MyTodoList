8a80b77e74b66fedf5feecfe58e4d114f20812fe
scarletrunner7000 <scarletrunner7000@gmail.com>
2016-08-12 22:59:47 +0900

    Chapter 5-04 まで完了

--- MyTodoList/ViewController.swift
+++ MyTodoList/ViewController.swift
@@ -8,7 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    var todoList = [String]()
+
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,6 +24,54 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func tapAddButton(sender: AnyObject) {
+        // アラートダイアログ作成
+        let alertController = UIAlertController(
+            title: "TODO 追加",
+            message: "TODO を入力してください",
+            preferredStyle: UIAlertControllerStyle.Alert)
+        // テキストエリア追加
+        alertController.addTextFieldWithConfigurationHandler(nil)
+        // OK ボタンを追加
+        let okAction = UIAlertAction(
+            title: "OK",
+            style: UIAlertActionStyle.Default,
+            handler: { (action:UIAlertAction) -> Void in
+                if let textField = alertController.textFields?.first {
+                    self.todoList.insert(textField.text!, atIndex: 0)
+
+                    // テーブルに行が追加されたことをテーブルに通知
+                    self.tableView.insertRowsAtIndexPaths(
+                        [NSIndexPath(forRow: 0, inSection: 0)],
+                        withRowAnimation: UITableViewRowAnimation.Right)
+                }
+            }
+        )
+        // OK ボタンを追加
+        alertController.addAction(okAction)
+
+        // CANCEL ボタンがタップされた時の処理
+        let cancelAction = UIAlertAction(
+            title: "CANCEL",
+            style: UIAlertActionStyle.Cancel,
+            handler: nil)
+        // CANCEL ボタンを追加
+        alertController.addAction(cancelAction)
+
+        // アラートダイアログを表示
+        presentViewController(alertController, animated: true, completion: nil)
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return todoList.count
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("todoCell", forIndexPath: indexPath)
+        let todoTitle = todoList[indexPath.row]
+        cell.textLabel!.text = todoTitle
+        return cell
+    }
 
 }
 
